6584ba651f06e13c4b57c2184a3367d53633b3c9
maxsaystransrights <maxmason2000@gmail.com>
2021-06-30 03:19:58 -0400

    some more warnings

--- src/components/home.js
+++ src/components/home.js
@@ -32,7 +32,16 @@ class Home extends React.Component {
                 <Container className="p-5">
                     <Jumbotron className="p-5 bg-light rounded">
                         <h2>Getting Started</h2>
-                        <p>This is a Discord spam bot tool. It has two main scripts:</p>
+                        <p>This is a Discord spam bot tool.</p>
+                        <Alert variant="info">
+                            <strong>🔧 Functionality Warning:</strong> Be sure to log in at least once from your IP address on the Discord account you're using. It <strong>cannot</strong> have 2FA.
+                        </Alert>
+                        <Alert variant="danger">
+                            <strong>🐞 Bug Warning:</strong> Inputting an invalid guild ID (w/ correct credentials) causes the scraper to hang, necessitating a refresh.
+                        </Alert>
+                        <Alert variant="warning">
+                            <strong>🦗 Bug Advisory:</strong> The spamming script will not validate your credentials, and the maximum run-time is <strong>1 day</strong>, as per <code>puppeteer-cluster</code>'s limitations.
+                        </Alert>
 
                         <Row>
                             <Col>
@@ -48,9 +57,6 @@ class Home extends React.Component {
                                             <Button className="mb-2" variant="danger" disabled>500 - Internal Server Error</Button> <br/>
                                             <span>If all goes well, you can download <code>users.txt</code> and proceed to the spammer tool. Otherwise, check your console.</span>
                                         </p>
-                                        <Alert variant="danger">
-                                            <strong>🐞 Bug Warning:</strong> Inputting an invalid guild ID (w/ correct credentials) causes the scraper to hang, necessitating a refresh.
-                                        </Alert>
                                     </Card.Body>
                                 </Card>
                             </Col>
@@ -67,9 +73,6 @@ class Home extends React.Component {
                                             <Button className="mb-2" variant="danger" disabled>500 - Internal Server Error</Button> <br/>
                                             <span>This script takes a while to complete, especially if your list is large, so please be patient! If it crashes, check your console.</span>
                                         </p>
-                                        <Alert variant="warning">
-                                            <strong>🦗 Bug Advisory:</strong> This script will not validate your credentials, and will not output any error on exit. The maximum run-time for this script is <strong>1 day</strong>, as per <code>puppeteer-cluster</code>'s limitations.
-                                        </Alert>
                                     </Card.Body>
                                 </Card>
                             </Col>
